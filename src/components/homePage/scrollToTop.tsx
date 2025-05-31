b17fbcbf1dd495820b2e32729ddb85d05b07bd74
Michael <ojomichael965@gmail.com>
2025-05-31 11:50:11 +0100

    final commit

--- src/components/homePage/scrollToTop.tsx
+++ src/components/homePage/scrollToTop.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useEffect } from "react"
 import { usePathname } from "next/navigation"
+import { log } from "console"
 
 export const ScrollToTop=()=>{
     const pathname = usePathname()
@@ -8,12 +9,14 @@ export const ScrollToTop=()=>{
     useEffect(()=>{
          const timeout = setTimeout(() => {
       // Try all possible scroll contexts
+       console.log('Body overflow:', document.body.style.overflow);
       window.scrollTo({ top: 0, behavior: "auto" });
       document.body.scrollTop = 0;
       document.documentElement.scrollTop = 0;
     }, 50); // Delay helps on iOS
 
-
+   
+    
     },[pathname]);
     
     return null
